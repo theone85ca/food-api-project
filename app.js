e13f4a6ca6546cdc9a28cdf9a0ce57ccf56b2175
theone85ca <theone85ca@gmail.com>
2018-02-19 12:36:59 -0500

    API and Testing setup

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const expressSession = require('express-session')({
+    secret: 'some random string goes here',
+    resave: false,
+    saveUninitialized: false
+});
+const User = require('./models/user');
+
+
 
 const index = require('./routes/index');
 const api = require('./routes/api/index');
+const users = require('./routes/api/users');
 
 const app = express();
+//Connect mongoose
+mongoose.connect('mongodb://localhost/musiclist')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,10 +34,19 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressSession);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/api', api);
+app.use('/api/users', users);
+
+// Configure passport
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
