5c7a379001607a96215261e466fe52b9d1dd5080
theone85ca <theone85ca@gmail.com>
2018-02-20 09:27:21 -0500

    WebPack and Hot-Reloading tweaks

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const expressSession = require('express-session')({
     resave: false,
     saveUninitialized: false
 });
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
 const User = require('./models/user');
 
 
@@ -39,9 +43,23 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// Webpack Server
+const webpackCompiler = webpack(webpackConfig);
+app.use(webpackDevMiddleware(webpackCompiler, {
+  publicPath: webpackConfig.output.publicPath,
+  stats: {
+    colors: true,
+    chunks: true,
+    'errors-only': true,
+  },
+}));
+app.use(webpackHotMiddleware(webpackCompiler, {
+  log: console.log,
+}));
+
 app.use('/api', api);
 app.use('/api/users', users);
+app.use('/*', index);
 
 // Configure passport
 passport.use(new LocalStrategy(User.authenticate()));
